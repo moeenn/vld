9618cfc40bb5a0ffa8698964a533c183c11a4daf
Muhammad Moeen <moeen.v8@gmail.com>
2024-03-07 12:57:33 +0500

    added readme

--- vld.go
+++ vld.go
@@ -6,40 +6,6 @@ import (
 	"strings"
 )
 
-/*
-TODO: Validators required
-- [x] NonEmptyString
-- [x] Length (exact)
-- [x] Min length
-- [x] Max length
-- [x] Min (number)
-- [x] Max (number)
-- [x] Less than (number)
-- [x] Greater than (number)
-- [x] Email
-- [ ] UUID
-- [ ] URL
-- [ ] Date
-- [ ] Time
-- [ ] Before date
-- [ ] Before or equal to date
-- [ ] After date
-- [ ] After or equal to date
-- [ ] Starts with
-- [ ] Doesn't start with
-- [ ] Ends with
-- [ ] Doesn't end with
-- [ ] Enum
-- [ ] Password
-- [ ] Base64
-- [ ] JSON
-- [ ] Regex
-- [ ] Same
-- [ ] Array
-- [ ] Min items
-- [ ] Max items
-*/
-
 type Rule func(any) error
 
 type Validation struct {
